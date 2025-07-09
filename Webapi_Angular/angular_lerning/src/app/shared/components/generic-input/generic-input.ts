280a32d27cb10a8c77293c70d1c422dd8c1eb167
VanshaTatvaSoft <tatva.pci217@outlook.com>
2025-07-09 16:35:13 +0530

    generic input

---
+++ Webapi_Angular/angular_lerning/src/app/shared/components/generic-input/generic-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-input',
+  imports: [],
+  templateUrl: './generic-input.html',
+  styleUrl: './generic-input.css'
+})
+export class GenericInput {
+
+}
